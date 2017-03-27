34b49aaf34d59302162b62fe7dbaa4344570758c
Amir Raminfar <findamir@gmail.com>
2017-03-27 09:43:43 -0700

    Adds deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+msg() {
+    printf "\033[1;32m%s\n\033[0m" "$1"
+}
+
+set -e
+
+msg "Updating docker-machine to clash..."
+eval $(docker-machine env --shell bash clash)
+
+msg "Deploying with docker..."
+docker-compose -f docker-compose.yml up -d --build
+
+msg "Success."

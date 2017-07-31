eaf22c052c827670c6239dcb856cdb3b2afd585e
Amir Raminfar <findamir@gmail.com>
2017-07-30 17:12:57 -0700

    Update clan.tag to tag

--- app/scheduler.py
+++ app/scheduler.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 def update_clans():
     logger.info("Updating all clans.")
-    for tag in Player.objects.distinct('clan.tag'):
+    for tag in Clan.objects.distinct('tag'):
         try:
             logger.info(f"Updating player stats for {tag}.")
             clan = api.find_clan_by_tag(tag)
@@ -39,6 +39,7 @@ schedule.every().day.at("1:30").do(update_clans)
 schedule.every().day.at("9:30").do(update_clans)
 schedule.every().day.at("17:30").do(update_clans)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    while True:
+        schedule.run_pending()
+        time.sleep(1)

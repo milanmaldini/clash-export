b33af043f8e12f5f3937ac8b1e113ac3e808f7bf
Amir Raminfar <amir.raminfar@capitalone.com>
2017-07-31 12:23:39 -0700

    Updates scheduler to every hour

--- app/scheduler.py
+++ app/scheduler.py
@@ -5,6 +5,8 @@ import os
 import schedule
 from mongoengine import connect
 from raven import Client
+from bson.objectid import ObjectId
+from datetime import datetime, timedelta
 
 import api
 from model import Player, Clan
@@ -17,9 +19,23 @@ connect(db='clashstats', host='db', connect=False)
 logger = logging.getLogger(__name__)
 
 
+def object_id_from_now(**kargs):
+    now = datetime.now()
+    dt = now - timedelta(**kargs)
+    return ObjectId.from_datetime(dt)
+
+
 def update_clans():
-    logger.info("Updating all clans.")
-    for tag in Clan.objects.distinct('tag'):
+    all_tags = set(Clan.objects.distinct('tag'))
+    already_done = set(Clan.objects(id__gte=object_id_from_now(hours=12)).distinct('tag'))
+    tags_to_fetch = list(all_tags - already_done)
+
+    total = len(tags_to_fetch)
+    tags_to_fetch = tags_to_fetch[:15]
+
+    logger.info(f"Fetching {len(tags_to_fetch)} of total {total} eligible items.")
+
+    for tag in tags_to_fetch:
         try:
             logger.info(f"Updating player stats for {tag}.")
             clan = api.find_clan_by_tag(tag)
@@ -34,10 +50,10 @@ def update_clans():
             logger.exception(f"Error while fetching clan {tag}.")
             client.captureException()
 
+    logger.info(f"Done fetching clans.")
 
-schedule.every().day.at("1:30").do(update_clans)
-schedule.every().day.at("9:30").do(update_clans)
-schedule.every().day.at("17:30").do(update_clans)
+
+schedule.every().hour.do(update_clans)
 
 if __name__ == "__main__":
     while True:

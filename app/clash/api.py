c8867c8523805c5249dd47b1530fa6fe0365c468
Amir Raminfar <findamir@gmail.com>
2017-08-04 08:05:05 -0700

    Adds logging

--- app/clash/api.py
+++ app/clash/api.py
@@ -2,10 +2,13 @@ import concurrent.futures
 from urllib.parse import quote
 
 import os
+import logging
 import requests
 
 token = os.getenv('API_TOKEN')
 headers = {'authorization': 'Bearer ' + token}
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def __get_with_threads(urls):
@@ -14,10 +17,12 @@ def __get_with_threads(urls):
 
 
 def find_clan_by_tag(tag):
+    logger.info(f"Fetching clan from API {tag}.")
     return requests.get('https://api.clashofclans.com/v1/clans/' + quote(tag), headers=headers).json()
 
 
 def fetch_all_players(clan):
+    logger.info(f"Fetching all player stats for {clan['tag']}.")
     tags = [member['tag'] for member in clan['memberList']]
     urls = ['https://api.clashofclans.com/v1/players/' + quote(tag) for tag in tags]
     return __get_with_threads(urls)
